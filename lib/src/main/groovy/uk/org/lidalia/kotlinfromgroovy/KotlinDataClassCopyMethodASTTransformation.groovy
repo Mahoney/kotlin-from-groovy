e242ec8f07ca644c89f2b8fa5371ae1300b91017
Robert Elliot <rob@lidalia.org.uk>
2022-10-17 14:19:20 +0100

    Added empty transformation

---
+++ lib/src/main/groovy/uk/org/lidalia/kotlinfromgroovy/KotlinDataClassCopyMethodASTTransformation.groovy
@@ -0,0 +1,18 @@
+package uk.org.lidalia.kotlinfromgroovy
+
+import groovy.transform.CompileStatic
+import org.codehaus.groovy.ast.ASTNode
+import org.codehaus.groovy.control.CompilePhase
+import org.codehaus.groovy.control.SourceUnit
+import org.codehaus.groovy.transform.ASTTransformation
+import org.codehaus.groovy.transform.GroovyASTTransformation
+
+@CompileStatic
+@GroovyASTTransformation(phase= CompilePhase.SEMANTIC_ANALYSIS)
+class KotlinDataClassCopyMethodASTTransformation implements ASTTransformation {
+
+    @Override
+    void visit(ASTNode[] nodes, SourceUnit source) {
+
+    }
+}
